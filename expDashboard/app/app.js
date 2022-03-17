06fe0639f03e99e8f85c8cddb639969e99a96f2a
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2022-03-17 05:59:21 +0530

    refactor

---
+++ expDashboard/app/app.js
@@ -0,0 +1,385 @@
+testData= [
+    {
+      Status: "Completed",
+      AssignedTo: "Anup",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "Marketing",
+      Added_Time_App: "22-Sep-2021 10:42:20",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "Whatsapp Integration",
+      Experiment_Type: "Product",
+      ID: "4004904000001142095",
+      Exp_Number: "6",
+      Goals: ""
+    },
+    {
+      Status: "Completed",
+      AssignedTo: "Anup",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "GPM",
+      Added_Time_App: "29-Sep-2021 13:28:36",
+      ETA: "",
+      Teams_Involved: "AM",
+      Title: "Cancellation flow change",
+      Experiment_Type: "Payments/Store Page",
+      ID: "4004904000001142091",
+      Exp_Number: "10",
+      Goals: "Reduce Churn of Customers"
+    },
+    {
+      Status: "Completed",
+      AssignedTo: "Nikhil",
+      Focus_Area: "Lead Generation",
+      Origin_Team: "GPM",
+      Added_Time_App: "26-Dec-2021 19:17:13",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "Trial User Behaviour Analysis for C6 Customers",
+      Experiment_Type: "Product",
+      ID: "4004904000001142087",
+      Exp_Number: "17",
+      Goals: "Lead Enrichment through In Product"
+    },
+    {
+      Status: "Completed",
+      AssignedTo: "Anup",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "GPM",
+      Added_Time_App: "02-Mar-2022 23:59:11",
+      ETA: "",
+      Teams_Involved: "AM",
+      Title: "Cx Portal Onboarding",
+      Experiment_Type: "Product",
+      ID: "4004904000001142083",
+      Exp_Number: "22",
+      Goals: ""
+    },
+    {
+      Status: "Completed",
+      AssignedTo: "Anup",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "GPM",
+      Added_Time_App: "18-Feb-2022 11:10:12",
+      ETA: "",
+      Teams_Involved: "Creator Store Team",
+      Title: "Premium/Enterprise support update to store for customers",
+      Experiment_Type: "Store",
+      ID: "4004904000001142079",
+      Exp_Number: "20",
+      Goals: ""
+    },
+    {
+      Status: "Completed",
+      AssignedTo: "",
+      Focus_Area: "Lead Generation",
+      Origin_Team: "GPM",
+      Added_Time_App: "07-Dec-2021 15:10:24",
+      ETA: "",
+      Teams_Involved: "Marketing",
+      Title: "C6 Setup Screen Phone field improvements",
+      Experiment_Type: "Product",
+      ID: "4004904000001142075",
+      Exp_Number: "15",
+      Goals: ""
+    },
+    {
+      Status: "In Progress",
+      AssignedTo: "Adil",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "GPM",
+      Added_Time_App: "07-Mar-2022 16:39:53",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "Graphical Content in the Setup Screen",
+      Experiment_Type: "Product",
+      ID: "4004904000001142071",
+      Exp_Number: "23",
+      Goals: ""
+    },
+    {
+      Status: "In Progress",
+      AssignedTo: "Anup",
+      Focus_Area: "Lead Generation",
+      Origin_Team: "GPM",
+      Added_Time_App: "06-Dec-2021 19:56:50",
+      ETA: "",
+      Teams_Involved: "Marketing",
+      Title: "Signup Page Merge to single page",
+      Experiment_Type: "Product",
+      ID: "4004904000001142067",
+      Exp_Number: "12",
+      Goals: ""
+    },
+    {
+      Status: "In Progress",
+      AssignedTo: "Anup",
+      Focus_Area: "Upselling",
+      Origin_Team: "GPM",
+      Added_Time_App: "24-Feb-2022 13:03:21",
+      ETA: "",
+      Teams_Involved: "AM",
+      Title: "Old pricing migration works",
+      Experiment_Type: "Product",
+      ID: "4004904000001142063",
+      Exp_Number: "21",
+      Goals: "Upgrade all customers to latest pricing"
+    },
+    {
+      Status: "In Progress",
+      AssignedTo: "Adil",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "GPM",
+      Added_Time_App: "17-Feb-2022 14:59:30",
+      ETA: "",
+      Teams_Involved: "Marketing",
+      Title: "Setup page - enhancement",
+      Experiment_Type: "Product",
+      ID: "4004904000001142059",
+      Exp_Number: "19",
+      Goals: ""
+    },
+    {
+      Status: "In Progress",
+      AssignedTo: "Anup",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "GPM",
+      Added_Time_App: "07-Dec-2021 13:25:44",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "C6 Homepage Help Chat - SalesIQ widget",
+      Experiment_Type: "Product",
+      ID: "4004904000001142055",
+      Exp_Number: "14",
+      Goals: ""
+    },
+    {
+      Status: "In Progress",
+      AssignedTo: "Udhay",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "Marketing",
+      Added_Time_App: "17-Feb-2022 14:57:04",
+      ETA: "",
+      Teams_Involved: "Marketing",
+      Title: "Whatsapp chat - salesiq",
+      Experiment_Type: "Desk/SalesIQ/Creator",
+      ID: "4004904000001142051",
+      Exp_Number: "18",
+      Goals: ""
+    },
+    {
+      Status: "In Progress",
+      AssignedTo: "",
+      Focus_Area: "",
+      Origin_Team: "",
+      Added_Time_App: "26-Apr-2021 10:36:24",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "Study of user activity in the current Landing Page",
+      Experiment_Type: "Study",
+      ID: "4004904000001142047",
+      Exp_Number: "0",
+      Goals: "Post Signup Engagement"
+    },
+    {
+      Status: "In Progress",
+      AssignedTo: "Anup",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "AM",
+      Added_Time_App: "07-Oct-2021 17:07:04",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "Banner to new subscribed user",
+      Experiment_Type: "Product",
+      ID: "4004904000001142043",
+      Exp_Number: "11",
+      Goals: "Customer engagement after Purchase"
+    },
+    {
+      Status: "In Development",
+      AssignedTo: "",
+      Focus_Area: "",
+      Origin_Team: "",
+      Added_Time_App: "26-Apr-2021 10:46:54",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "Feature Based Lead Nurturing Promotions for Trial Users",
+      Experiment_Type: "Promotion",
+      ID: "4004904000001142039",
+      Exp_Number: "2",
+      Goals: "Post Signup Engagement"
+    },
+    {
+      Status: "In Development",
+      AssignedTo: "",
+      Focus_Area: "",
+      Origin_Team: "",
+      Added_Time_App: "26-Apr-2021 10:43:25",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "Top cross sell usecases based on other Zoho Products",
+      Experiment_Type: "Study",
+      ID: "4004904000001142035",
+      Exp_Number: "1",
+      Goals: "Cross Selling"
+    },
+    {
+      Status: "In Development",
+      AssignedTo: "Anup",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "Partner",
+      Added_Time_App: "28-Sep-2021 15:03:23",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "Partner solutions inproduct banner",
+      Experiment_Type: "Product",
+      ID: "4004904000001142031",
+      Exp_Number: "9",
+      Goals: ""
+    },
+    {
+      Status: "In Development",
+      AssignedTo: "Nikhil",
+      Focus_Area: "Upselling",
+      Origin_Team: "GPM",
+      Added_Time_App: "13-Dec-2021 13:04:24",
+      ETA: "",
+      Teams_Involved: "AM",
+      Title: "V1-V4 Pricing Migration",
+      Experiment_Type: "CRM/Store/UserInfo",
+      ID: "4004904000001142027",
+      Exp_Number: "16",
+      Goals: "Upgrade all customers to latest pricing"
+    },
+    {
+      Status: "In Development",
+      AssignedTo: "Anup",
+      Focus_Area: "Lead Generation",
+      Origin_Team: "Marketing",
+      Added_Time_App: "21-Sep-2021 19:10:27",
+      ETA: "",
+      Teams_Involved: "Marketing",
+      Title: "Mandatory Phone number and Other info C6",
+      Experiment_Type: "Product",
+      ID: "4004904000001142023",
+      Exp_Number: "3",
+      Goals: "Lead Enrichment through In Product"
+    },
+    {
+      Status: "Not Started",
+      AssignedTo: "Nikhil",
+      Focus_Area: "Customer Engagement",
+      Origin_Team: "GPM",
+      Added_Time_App: "21-Sep-2021 19:18:20",
+      ETA: "",
+      Teams_Involved: "Marketing",
+      Title: "Customer Signup - Emails and Automations",
+      Experiment_Type: "Study",
+      ID: "4004904000001142019",
+      Exp_Number: "5",
+      Goals: ""
+    },
+    {
+      Status: "Not Started",
+      AssignedTo: "Mayuri",
+      Focus_Area: "Lead Generation",
+      Origin_Team: "Marketing",
+      Added_Time_App: "07-Dec-2021 13:01:21",
+      ETA: "",
+      Teams_Involved: "Marketing",
+      Title: "C6 Hackathon - Lead Source param update",
+      Experiment_Type: "Product",
+      ID: "4004904000001142015",
+      Exp_Number: "13",
+      Goals: ""
+    },
+    {
+      Status: "On Hold",
+      AssignedTo: "Nikhil",
+      Focus_Area: "Upselling",
+      Origin_Team: "GPM",
+      Added_Time_App: "23-Sep-2021 10:13:58",
+      ETA: "",
+      Teams_Involved: "AM",
+      Title: "v3/v4 pricing migration",
+      Experiment_Type: "MICS",
+      ID: "4004904000001142011",
+      Exp_Number: "8",
+      Goals: "Upgrade all customers to latest pricing"
+    },
+    {
+      Status: "Not Started",
+      AssignedTo: "Nikhil",
+      Focus_Area: "",
+      Origin_Team: "Marketing",
+      Added_Time_App: "21-Sep-2021 19:16:04",
+      ETA: "22-Sep-2021",
+      Teams_Involved: "",
+      Title: "App Creation Stats",
+      Experiment_Type: "Study",
+      ID: "4004904000001142007",
+      Exp_Number: "4",
+      Goals: ""
+    },
+    {
+      Status: "",
+      AssignedTo: "Udhay",
+      Focus_Area: "Upselling",
+      Origin_Team: "GPM",
+      Added_Time_App: "22-Sep-2021 17:12:50",
+      ETA: "",
+      Teams_Involved: "",
+      Title: "v1/v2 pricing migration works",
+      Experiment_Type: "Desk/SalesIQ/Creator",
+      ID: "4004904000001142003",
+      Exp_Number: "7",
+      Goals: "Upgrade all customers to latest pricing"
+    }
+  ]
+
+const StatusCountHandler = (status)=>{
+ let counter = 0; 
+ testData.filter((i)=>{
+   if(i.Status==status){
+       counter += 1;
+   }
+ 
+
+ });
+
+ let sclass = document.getElementById(status);
+ let sspan = document.createElement('span');
+ sspan.innerHTML = counter ;
+ sclass.appendChild(sspan);
+ 
+ 
+}
+
+window.addEventListener('load',()=>{
+    let divContainer = document.getElementById('dashboard-container');
+   
+    StatusCountHandler("Completed");
+});
+
+
+
+
+ZOHO.CREATOR.init()
+        .then(function(data) {
+          
+            var config = { 
+                appName : "Sample1-new",
+                reportName : "All_Experiments" 
+            
+            }
+            
+            ZOHO.CREATOR.API.getAllRecords(config).then(function(response){
+                var recordArr = response.data;
+                console.log(recordArr);
+            });
+
+
+
+        });
+
+
